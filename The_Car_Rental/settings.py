7a8da57402f55eb9a5f18589eb708e3f47fd2fe2
Daniyal Ahmed Sethi <daniyalahmedsethi@gmail.com>
2024-08-16 21:20:05 +0500

    3

--- The_Car_Rental/settings.py
+++ The_Car_Rental/settings.py
@@ -183,8 +183,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
