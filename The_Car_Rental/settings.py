1d2226ae433e3555e25a1962e4fa2f08635d61a9
Daniyal Ahmed Sethi <daniyalahmedsethi@gmail.com>
2024-08-29 12:46:10 +0500

    checkout

--- The_Car_Rental/settings.py
+++ The_Car_Rental/settings.py
@@ -145,8 +145,6 @@ DATABASES = {
         'PORT': '3306',
 
 
-        
-
     }
 }
 
@@ -213,5 +211,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 
 
